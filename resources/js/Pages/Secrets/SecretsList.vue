c5d0c1b7b6d41fb2f41ba3b1a1e880c4f136e109
Moose <hendrcor+1@gmail.com>
2026-07-28 18:52:36 -0500

    styling improvements

--- resources/js/Pages/Secrets/SecretsList.vue
+++ resources/js/Pages/Secrets/SecretsList.vue
@@ -1,5 +1,7 @@
 <script setup>
 import SecretDisplay from '@/Components/SecretDisplay.vue'
+import SectionBorder from '@/Components/SectionBorder.vue';
+
 const props = defineProps({
     activeSecrets: Array,
     inactiveSecrets: Array,
@@ -9,23 +11,25 @@ const props = defineProps({
 
 <template>
     <div v-if="props.activeSecrets && props.activeSecrets.length > 0">
-        <h1 class="font-bold text-xl">Active Secrets</h1>
+        <h1 class="font-bold text-xl ">Active Secrets</h1>
         <div class="w-full flex flex-row px-6 py-4 gap-2">
-            <span class="w-2/5">Message</span>
-            <span class="w-3/5">Link</span>
-            <span class="w-1/6 text-end">Expires At</span>
+            <span class="w-2/5 text-gray-500">Message</span>
+            <span class="w-3/5 text-gray-500">Link</span>
+            <span class="w-1/6 text-end text-gray-500">Expires At</span>
         </div>
         <div v-for="secret in props.activeSecrets">
             <SecretDisplay :secret="secret"/>
         </div>
     </div>
 
+    <SectionBorder />
+
     <div v-if="props.inactiveSecrets && props.inactiveSecrets.length > 0">
         <h1 class="font-bold text-xl">Inactive Secrets</h1>
         <div class="w-full flex flex-row px-6 py-4 gap-2">
-            <span class="w-2/5">Message</span>
-            <span class="w-3/5">Link</span>
-            <span class="w-1/6 text-end">Expires At</span>
+            <span class="w-2/5 text-gray-500">Message</span>
+            <span class="w-3/5 text-gray-500">Link</span>
+            <span class="w-1/6 text-end text-gray-500">Expires At</span>
         </div>
         <div v-for="secret in props.inactiveSecrets">
             <SecretDisplay :secret="secret"/>
--- resources/js/Pages/Secrets/SecretsView.vue
+++ resources/js/Pages/Secrets/SecretsView.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col p-4 border-2 bg-gray-300 border-black-500 rounded-lg">
         <span>You have a message from <span v-text="senderName"/>, please read or copy down before exiting page! Message will not be viewable again!</span>
         <span class="grow mt-2 border-2 bg-blue-300 border-blue-500 rounded-lg p-4" v-text="props.message"/>
     </div>
