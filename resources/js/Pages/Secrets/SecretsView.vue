193aafd2dfdb5c236bfa4aba8da13b3a981e148b
Moose <hendrcor+1@gmail.com>
2026-07-28 19:25:49 -0500

    Fixed weird language

--- resources/js/Pages/Secrets/SecretsView.vue
+++ resources/js/Pages/Secrets/SecretsView.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 
 <template>
     <div class="flex flex-col p-4 border-2 bg-gray-300 border-black-500 rounded-lg">
-        <span>You have a message from <span v-text="senderName"/>, please read or copy down before exiting page! Message will not be viewable again!</span>
+        <span>You have a message from <span v-text="senderName"/>, please read or copy down before exiting the page. This message will not be viewable again after exiting!</span>
         <span class="grow mt-2 border-2 bg-blue-300 border-blue-500 rounded-lg p-4" v-text="props.message"/>
     </div>
 </template>
